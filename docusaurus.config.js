f17c6694d154c5d2f52cf289e78a2b7f34a0f687
critesjosh <jc@joshcrites.com>
2023-10-22 10:57:57 -0400

    make preview png

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/bird.svg",
+      image: "img/bird.png",
       colorMode: {
         defaultMode: "light",
         disableSwitch: true,
