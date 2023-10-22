70137b6a5f9f86f92906b5a0b602bddde16cae6f
critesjosh <jc@joshcrites.com>
2023-10-21 22:16:22 -0400

    udpate image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Bank of JubJub",
   tagline: "Privacy is cool",
-  favicon: "img/favicon.ico",
+  favicon: "img/bird.svg",
 
   // Set the production url of your site here
   url: "https://bankofjubjub.com",
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/bird.svg",
       colorMode: {
         defaultMode: "light",
         disableSwitch: true,
