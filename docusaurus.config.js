70cc1a826365d9d9083a408dfe063998f8af2c22
josh crites <jc@joshcrites.com>
2023-11-04 20:11:14 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/bank-of-jubjub/docs/tree/main/",
+            "https://github.com/bank-of-jubjub/docs/blob/master",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
